35520d0f22ed1b51ac2bad8883f4aaed06d5a5db
ryxmarcus <128896074+ryxmarcus@users.noreply.github.com>
2024-06-29 17:41:56 +0530

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-1234567"
+    bucket = "dev-proj-1-remote-state-bucket-12345678"
     key    = "devops-project-1/terraform.tfstate"
     region = "eu-central-1"
   }
