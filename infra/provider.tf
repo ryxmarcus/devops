fbdb23ff01e74d4cf281d46840db2608f69ce027
ryxmarcus <128896074+ryxmarcus@users.noreply.github.com>
2024-06-30 09:39:25 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
   region                   = "eu-central-1"
-  AWS_ACCESS_KEY_ID     = credentials('AWS_ACCESS_KEY_ID')
-  AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+  shared_credentials_files = ["/Users/hk587/.aws/credentials"]
 }
